0c7f4cdf6cec6839ce98af90ca08820b47d6c9f7
Benjamin Ryzman <benjamin@ryzman.fr>
2025-12-13 12:44:00 +0100

    Update LazyVim config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local set = vim.keymap.set
+set("i", "jj", "<Esc>", { noremap = false })
+
+local map = function(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  set(mode, lhs, rhs, options)
+end
+map("i", "<C-r>+", "<C-r><C-o>+", { desc = "Insert clipboard, keeping indentation" })
+map("i", "<S-Insert>", "<C-r><C-o>*", { desc = "Insert primary clipboard, keeping indentation" })
+set("c", "<S-Insert>", "<C-r>*", { desc = "Insert primary clipboard, keeping indentation" })
+map("i", "<C-r>*", "<C-r><C-o>*", { desc = "Insert primary clipboard, keeping indentation" })
+map("v", "<C-Insert>", '"*y')
+map("v", "<S-Delete>", '"*d')
