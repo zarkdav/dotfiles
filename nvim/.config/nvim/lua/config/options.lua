83443482fd00652677bb2805e0d1e4dba76621f0
Benjamin Ryzman <benjamin@ryzman.fr>
2025-12-13 13:30:25 +0100

    Start neovide fullscreen

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,7 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
+
+if vim.g.neovide then
+  vim.g.neovide_fullscreen = true
+end
